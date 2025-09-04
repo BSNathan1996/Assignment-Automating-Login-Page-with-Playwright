3c72045deb95a608213175cc283122b690e2b478
BSNathan1996 <bsnathan61@gmail.com>
2025-09-04 11:35:32 +0530

    quick beautify and adding proper comments for visibility

--- action/Login/login-action.ts
+++ action/Login/login-action.ts
@@ -11,28 +11,33 @@ export class LoginAction {
         this.helper = new helper(page);
     }
 
+    // function to verify title of login page
     public async verifyTitleLoginPage(testData: Dictionary<string>){
         const title = await this.page.title();
         await expect(title).toBe(testData.title);
     }
 
+    // function to enter Username data in the username field
     public async verifyUserNameData(testData: Dictionary<string>){
         const userNameTextBox = await this.page.locator(loginViews.userNameTextBox);
         await expect(userNameTextBox).toBeVisible();
         userNameTextBox.fill(testData.username);
     }
 
+    // function to enter Password data in the password field
     public async verifyPasswordData(testData: Dictionary<string>){
         const passwordTextBox = await this.page.locator(loginViews.passwordTextBox);
         await expect(passwordTextBox).toBeVisible();
         passwordTextBox.fill(testData.password);
     }
 
+    // function to click on submit button
     public async verifySubmitButton(){
         const submitButton = await this.page.locator(loginViews.submitButton);
         await submitButton.click();
     }
 
+    // function to get the error msg after click on submit button
     public async verifySubmitError(testData:Dictionary<string>){
         const submitButton = await this.page.locator(loginViews.submitButton);
         await submitButton.click();
@@ -40,7 +45,8 @@ export class LoginAction {
         console.log(errorMessage)
         await expect(errorMessage).toBe(testData.errMsg);
     }
-
+    
+    // function to get the error msg after click on the submit button with invalid credential
     public async verifySubmitInvalidCred(testData: Dictionary<string>){
         const submitButton = await this.page.locator(loginViews.submitButton);
         await submitButton.click();
@@ -49,18 +55,20 @@ export class LoginAction {
         await expect(errorMessage).toBe(testData.errMsg);
     }
 
-
+    // function to verify login page elemets
     public async verifyLoginPageElement(){
         await this.helper.isVisible(loginViews.userNameTextBox);
         await this.helper.isVisible(loginViews.passwordTextBox);
         await this.helper.isEnabled(loginViews.submitButton);
     }
 
+    // function to verify success login page title
     public async verifySuccessPageTitle(testData:Dictionary<string>){
         const title = await this.page.title();
         await expect(title).toBe(testData.title);
     }
 
+    // function to verify success login page elements
     public async verifySuccessPageElement(testData: Dictionary<string>){
         await expect(this.page.url()).toBe(testData.successPageURL);
         await this.helper.textMatch(loginSuccessViews.successfullLoginHeaderByClass, testData.header);
@@ -71,6 +79,7 @@ export class LoginAction {
         await expect(logoutBtn).toBeVisible();
     }
 
+    // function to verify the logout button functionality
     public async verifyLogoutFunctionality(testData: Dictionary<string>){
         const logoutBtn = await this.page.locator('a').filter({
             hasText: loginSuccessViews.logoutButtonByText
--- common/helper.ts
+++ common/helper.ts
@@ -7,26 +7,32 @@ export class helper {
         this.page = page;
     }
 
+    // helper function to get the text from the element
     public async getText(element: string){
         const textOf = await this.page.locator(element).textContent();
         return textOf;
     }
 
+    // helper function to verify if the element is visible
     public async isVisible(element: string){
         const isElementVisible = await this.page.locator(element);
         await expect(isElementVisible).toBeVisible();
     }
 
+    // helper function to verify if the element is not disabled
     public async isEnabled(element: string){
         const isElementEnabled = await this.page.locator(element);
-        await expect(isElementEnabled).toBeEnabled();
+        await expect(isElementEnabled).not.toBeDisabled();
     }
 
+    // helper function to match the text
     public async textMatch(element: string, text: string){
         const textOf = await this.page.locator(element);
         await expect(textOf).toHaveText(text);
     }
 }
+
+// an interface to declare a Dictionary data type which accepts Generics<T> values
 export interface Dictionary<T> {
     [key: string]: T;
 }
